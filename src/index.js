0fe0d708f6f7d68035542357e7cffc61545cb3ea
Philippe <ducassephi@hotmail.fr>
2023-07-14 20:47:38 +0200

    added atatus CI

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('70c717fb4a844261b4758f0d9d64b812').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
